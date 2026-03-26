d94b2e994a29368fe41c2107500a010d306e8ce1
André Vinícius Diz Freitas <114784247+TheAndr3@users.noreply.github.com>
2026-03-25 23:11:42 -0300

    Add perceptron implementation and training data files

---
+++ perceptron.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+ETA = 0.01  # Taxa de aprendizagem 
+X0 = -1     # Bias 
+
+def funcao_ativacao(u):
+    #Convenção: 1 para C2 e 0 para C1 
+    return 1 if u >= 0 else 0
+
+def treinar_perceptron(entradas, saídas_desejadas):
+    pesos = np.random.rand(3) 
+    pesos_iniciais = pesos.copy()
+    
+    epocas = 0
+    while True:
+        erros_na_epoca = 0
+        epocas += 1
+        
+        for i in range(len(entradas)):
+            x = np.array([X0, entradas[i][0], entradas[i][1]])
+            d = saídas_desejadas[i]
+            
+            # (u) 
+            u = np.dot(x, pesos)
+            
+            y = funcao_ativacao(u)
+
+            #Aprendizado 
+            if y != d:
+                erro = d - y
+                pesos = pesos + ETA * erro * x  #Regra de Hebb
+                erros_na_epoca += 1
+        
+        #  treino para quando não houver mais erros na época
+        if erros_na_epoca == 0 or epocas > 1000:
+            break
+    
+    return pesos_iniciais, pesos, epocas
+    
+dados_treino = np.loadtxt('dados-tra.txt')
+X_treino = dados_treino[:, :2]
+d_treino = dados_treino[:, 2]
+
+for t in range(1, 6):
+    w_init, w_final, total_epocas = treinar_perceptron(X_treino, d_treino)
+    print(f"Treino {t}: Épocas = {total_epocas} | Pesos Finais: {w_final}")
+    np.savetxt('pesos_finais.txt', w_final, fmt='%.4f')
